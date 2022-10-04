39d101c591d5fee6fd2a08188c9dfb4e764d15b8
kristinbaker1 <paletteofpink@gmail.com>
2022-10-04 17:36:18 -0400

    Animation commit

--- My project/Assets/Script/NewBehaviour.cs
+++ My project/Assets/Script/NewBehaviour.cs
@@ -8,49 +8,58 @@ public class NewBehaviour : MonoBehaviour
 public AudioClip musicClipTwo;
 
 public AudioSource musicSource;
+Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-         if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
-
+          anim.SetInteger("State", 1);
          }
 
-     if (Input.GetKeyUp(KeyCode.W))
+        if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
          }
          
-     if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
          }
 
-     if (Input.GetKeyUp(KeyCode.R))
+        if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
+          anim.SetInteger("State", 0);
 
          }
+          if (Input.GetKeyDown(KeyCode.L))
 
-     if (Input.GetKeyDown(KeyCode.L))
         {
+
           musicSource.loop = true;
+
          }
 
      if (Input.GetKeyUp(KeyCode.L))
+
         {
+
           musicSource.loop = false;
+
         }
+
     }
 }
